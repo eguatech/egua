5ba39b652fdf3e7aae672a67966871cfde2fd939
lucaspompeun <lucaspompeuneves@gmail.com>
2020-04-15 20:57:11 -0300

    refatorando importar biblioteca

--- src/lib/importStdlib.js
+++ src/lib/importStdlib.js
@@ -5,7 +5,9 @@ const loadModule = function (modulePath) {
 
     let keys = Object.keys(moduleData);
     for (let i = 0; i < keys.length; i++) {
-        moduleData[keys[i]] = new StandardFN(moduleData[keys[i]].length, moduleData[keys[i]]);
+        let currentFunc = moduleData[keys[i]];
+
+        moduleData[keys[i]] = new StandardFN(currentFunc.length, currentFunc);
     }
 
     return moduleData;
