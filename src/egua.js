3a9ff1e9f53b3a779af803b8ed710f55fec492ae
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-08-07 04:19:04 +0000

    atualização de console

--- src/egua.js
+++ src/egua.js
@@ -17,10 +17,11 @@ module.exports.Egua = class Egua {
 
     runPrompt() {
         const interpreter = new Interpreter(this, process.cwd(), undefined);
+        console.log("Console da Linguagem Egua v1.0.4");
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
-            prompt: ">>> "
+            prompt: "\negua> "
         });
 
         rl.prompt();
