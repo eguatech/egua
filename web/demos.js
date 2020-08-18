3cd1a806f705249eb6abb1b2c404b96a78782c87
Heictor <39010251+Heictor@users.noreply.github.com>
2020-08-18 20:48:24 -0300

    Inserindo exemplo de merge sort

--- web/demos.js
+++ web/demos.js
@@ -20,4 +20,54 @@ const demos = {
   classe
 }
 
-
+const MergeSort = 
+var vetor1 = [8, 2, 9, 5];
+var a=0;
+var aux=0;
+var i=0;
+escreva ("Vetor: Posição[0]:"+texto(vetor1[0]));
+escreva ("Vetor: Posição[1]:"+texto(vetor1[1]));
+escreva ("Vetor: Posição[2]:"+texto(vetor1[2]));
+escreva ("Vetor: Posição[3]:"+texto(vetor1[3]));
+para (i=0; i<3; i=i+1){
+  se (vetor1[i]>vetor1[i+1]){
+    
+    escreva ("Vetor "+texto(i));
+    aux = vetor1[i];
+    vetor1[i] = vetor1[i+1];
+    vetor1[i+1] = aux;
+    escreva(vetor1[i]);
+    escreva(vetor1[i+1]);
+  }
+}
+var vetor2 = [vetor1[0], vetor1[1]];
+var vetor3 = [vetor1[2], vetor1[3]];
+var vetor4 = [];
+para (a=0; a<4; a=a+1){
+  escreva ("vetor1("+texto(a)+")");
+  escreva (vetor1[a]);
+}
+para (a=0; a<2; a=a+1){
+  escreva ("vetor2("+texto(a)+")");
+  escreva (vetor2[a]);
+}
+para (a=0; a<2; a=a+1){
+  escreva ("vetor3("+texto(a)+")");
+  escreva (vetor3[a]);
+}
+se (vetor2[0]<vetor3[0] e vetor2[1]<vetor3[1]){
+  vetor4[0]=vetor2[0];
+  se (vetor3[0]<vetor2[1]){
+  vetor4[1]=vetor3[0];
+  vetor4[2]=vetor2[1];
+  vetor4[3]=vetor3[1];
+  }senao{
+  vetor4[1]=vetor2[1];
+  vetor4[2]=vetor3[0];
+  vetor4[3]=vetor3[1];
+  }
+}
+    
+para (a=0; a<4; a=a+1){
+  escreva ("vetor4("+texto(vetor4[a])+")");
+}
