0cb4cb3d6415fc010e4e72bf17c1b7c601b2744b
Julio Azancort <45800951+Jazancort@users.noreply.github.com>
2020-08-18 21:46:48 -0300

    Adição de Bhaskara

--- web/demos.js
+++ web/demos.js
@@ -4,13 +4,11 @@ const Classe = `classe Animal {
       escreva("Correndo Loucamente");
   }
 }
-
 classe Cachorro herda Animal {
   latir() {
       escreva("Au Au Au Au");
   }
 }
-
 var nomeDoCachorro = Cachorro();
 nomeDoCachorro.correr();
 nomeDoCachorro.latir();`
@@ -66,8 +64,51 @@ para (a=0; a<4; a=a+1){
   escreva ("vetor4("+texto(vetor4[a])+")");
 }`
 
+const Bhaskara = `var a; var b; var c; var d; var c; var f; var x1; var x2; var r1; var r2;
+
+funcao bhaskara(d,a,b,c,x1,x2){
+  //A variável "d" vai simbolizar o Delta.
+  //"a", "b", e "c" irão representar os coeficientes da equação.
+  d = b**2;
+        var f = 4*a*c; 
+  d = d-f;
+  escreva("O valor de Delta é: " + texto(d));
+  d = d**0.5;
+  
+  //Encontrando os valores de X1 e X2.
+  x1 = -b+d;
+  x1 = x1/2*a;
+  escreva("O valor de X1 é: "+ texto(x1));
+  x2 = -b-d;
+  x2 = x2/2*a;
+  escreva("O valor de X2 é: "+ texto(x2));
+  //Resultado das substituições de X por X1 e X2 na equação.
+  var r1 = x1**2;
+  r1 = a*r1;
+  r1 = b*x1 + r1;
+  r1 = r1 + c;
+  escreva("Substituindo X1 na equação obtém-se:"+ texto(r1));
+  var r2 = x2**2;
+  r2 = a*r2;
+  r2 = b*x2 + r2;
+  r2 = r2 + c;
+  escreva("Substituindo X2 na equação obtém-se:"+ texto(r2));
+}
+
+//Insira o valor do coeficiente A:
+ var a = 1;
+
+//Insira o valor do coeficiente B:
+ var b = -1;
+
+//Insira o valor do coeficiente B:
+ var c = -30;
+
+bhaskara(d,a,b,c,x1,x2);`
+
 const demos = {
   Exemplos,
   Classe,
-  MergeSort
+  MergeSort,
+  Bhaskara
 }
