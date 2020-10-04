96c42cd9581949390fada9f7af8402b93145214e
Fernando Barboza <falandocomofernando@gmail.com>
2020-10-04 19:29:25 -0300

    adicionado novos metodos de tempo

--- src/lib/time.js
+++ src/lib/time.js
@@ -2,7 +2,19 @@ module.exports.time = function () {
     return +new Date();
 };
 
-module.exports.hora = function (timestamp) {
+module.exports.segundos = function () {
+    return new Date().getSeconds();
+};
+
+module.exports.minutos = function () {
+    return new Date().getMinutes();
+};
+
+module.exports.horas = function () {
+    return new Date().getHours();
+};
+
+module.exports.horario = function (timestamp) {
     let timeFormatted = timestamp !== null ? new Date(timestamp) : new Date();
     return timeFormatted;
 };
