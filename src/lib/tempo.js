c9c7f29de9c8769c1e31616959556c39e1cf864e
Fernando Barboza <falandocomofernando@gmail.com>
2021-02-03 22:15:38 -0300

    refatoração metodo horario #88, adicionado comentarios

--- src/lib/tempo.js
+++ src/lib/tempo.js
@@ -1,22 +1,46 @@
-const { RuntimeError } = require("../errors");
+const RuntimeError = require("../errors.js").RuntimeError;
 
-module.exports.time = function () {
-    return +new Date();
+// Retorna uma data completa
+module.exports.tempo = function () {
+	return new Date();
 };
 
+// Retorna os segundos atuais do sistema
 module.exports.segundos = function () {
-    return new Date().getSeconds();
+	return new Date().getSeconds();
 };
 
+// Retorna os minutos atuais do sistema
 module.exports.minutos = function () {
-    return new Date().getMinutes();
+	return new Date().getMinutes();
 };
 
+// Retorna a hora atual do sistema
 module.exports.horas = function () {
-    return new Date().getHours();
+	return new Date().getHours();
 };
 
+// Retorna a data completa que foi passada por parâmetro
 module.exports.horario = function (timestamp) {
-    let timeFormatted = timestamp !== null ? new Date(timestamp) : new Date();
-    return timeFormatted;
-};
+	const regex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+
+	if (typeof timestamp !== 'string' || !regex.test(timestamp)){
+		throw new RuntimeError(
+			this.token,
+			"O parâmetro passado deve ser um texto com a data no formato PT-BR."
+		);
+	}
+	
+	const date = new Date(convertDateFromPtbrToISO(timestamp));
+	const timezoneOffset = date.getTimezoneOffset();
+
+	return new Date(date.getTime() + timezoneOffset * 60 * 1000);
+}
+
+function convertDateFromPtbrToISO(date) {
+  const day  = date.split("/")[0];
+  const month  = date.split("/")[1];
+  const year  = date.split("/")[2];
+
+	return `${year}-${month}-${day}`;
+}
